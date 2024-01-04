18740af76baf0144a0cf87040a0161790b96a5fd
Anthony <anthony.vergne@outlook.com>
2024-01-04 18:02:12 -0500

    App : Remove login page from nav bar & update icons

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -6,22 +6,28 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginComponent } from './pages/login/login.component';
 import { inject } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    canActivate: [() => inject(AuthService).isLoggedIn()],
-    component: HomeComponent,
-  },
   {
     path: 'login',
     canActivate: [() => inject(AuthService).shouldLogIn()],
     component: LoginComponent,
   },
   {
-    path: 'about',
+    path: '',
     canActivate: [() => inject(AuthService).isLoggedIn()],
-    component: AboutComponent,
+    component: NavBarComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      { path: '**', component: NotFoundComponent },
+    ],
   },
-  { path: '**', component: NotFoundComponent },
 ];
